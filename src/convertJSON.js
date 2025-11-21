656e88976fcf8a4ee087429c4284f091c7d04638
Erik Chao <etchao@ucsc.edu>
2025-11-21 14:14:51 -0800

    Adding CSV to JSON conversion for explore page

---
+++ src/convertJSON.js
@@ -0,0 +1,19 @@
+import Papa from "papaparse";
+
+export async function loadCsvAsJson() {
+    const response = await fetch("/Taxonomy Observations - Obvservations.csv");
+    const csvText = await response.text();
+
+    const parsed = Papa.parse(csvText, {
+        header: true,
+        skipEmptyLines: true
+    });
+
+    const jsonObject = parsed.data;
+    return jsonObject;
+}
+
+// Example usage
+loadCsvAsJson().then(data => {
+    console.log("JSON Object:", data);
+});
