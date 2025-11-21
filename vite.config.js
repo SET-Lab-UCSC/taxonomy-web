559a2a27996037be1e91ae36a028fd7f8461c0f1
Erik Chao <etchao@ucsc.edu>
2025-11-21 15:41:53 -0800

    Adding all html files onto docs

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,12 @@ export default {
         index: resolve(__dirname, 'src/index.html'),   // include your main page
         tasks: resolve(__dirname, 'src/tasks.html'),
         explore: resolve(__dirname, 'src/explore.html'),
+        applications: resolve(__dirname, 'src/applications.html'),
+        create: resolve(__dirname, 'src/create.html'),
+        environment: resolve(__dirname, 'src/environmental-change.html'),
+        feedback: resolve(__dirname, 'src/feedback.html'),
+        fruit: resolve(__dirname, 'src/fruit-ninja.html'),
+        interface: resolve(__dirname, 'src/interface-elements.html'),
 
       },
     },
