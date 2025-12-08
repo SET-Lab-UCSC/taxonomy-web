c8528f988c376176e2a51b081124d781519a3542
Erik Chao <etchao001@gmail.com>
2025-12-07 23:55:02 -0800

    Adding modal support for categories

--- src/convertJSON.js
+++ src/convertJSON.js
@@ -26,6 +26,73 @@ export async function loadAppsAsJson() {
     return jsonObject;
 }
 
+export async function loadFeedAsJson() {
+    const response = await fetch("./Taxonomy Observations - Feedback.csv");
+    const csvText = await response.text();
+
+    const parsed = Papa.parse(csvText, {
+        header: true,
+        skipEmptyLines: true
+    });
+
+    const jsonObject = parsed.data;
+    return jsonObject;
+}
+
+export async function loadInputsAsJson() {
+    const response = await fetch("./Taxonomy Observations - Inputs.csv");
+    const csvText = await response.text();
+
+    const parsed = Papa.parse(csvText, {
+        header: true,
+        skipEmptyLines: true
+    });
+
+    const jsonObject = parsed.data;
+    return jsonObject;
+}
+
+export async function loadTechAsJson() {
+    const response = await fetch("./Taxonomy Observations - Interaction Techniques.csv");
+    const csvText = await response.text();
+
+    const parsed = Papa.parse(csvText, {
+        header: true,
+        skipEmptyLines: true
+    });
+
+    const jsonObject = parsed.data;
+    return jsonObject;
+}
+
+export async function loadFaceAsJson() {
+    const response = await fetch("./Taxonomy Observations - Interface Elements.csv");
+    const csvText = await response.text();
+
+    const parsed = Papa.parse(csvText, {
+        header: true,
+        skipEmptyLines: true
+    });
+
+    const jsonObject = parsed.data;
+    return jsonObject;
+}
+
+export async function loadTasksAsJson() {
+    const response = await fetch("./Taxonomy Observations - Tasks.csv");
+    const csvText = await response.text();
+
+    const parsed = Papa.parse(csvText, {
+        header: true,
+        skipEmptyLines: true
+    });
+
+    const jsonObject = parsed.data;
+    return jsonObject;
+}
+
+
+
 // Example usage
 // loadCsvAsJson().then(data => {
 //     console.log("JSON Object:", data);
