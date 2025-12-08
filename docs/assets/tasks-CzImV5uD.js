553bf15b9fa518b93accdd3a41bdf7df9b13a340
Erik Chao <etchao001@gmail.com>
2025-12-08 00:03:57 -0800

    Adding tasks modal

---
+++ docs/assets/tasks-CzImV5uD.js
@@ -0,0 +1,41 @@
+import "./modulepreload-polyfill-DaKOjhqt.js";
+/* empty css                */
+import { l as loadObsAsJson, a as loadTasksAsJson } from "./csvtojson-CHK6WrLd.js";
+document.addEventListener("DOMContentLoaded", async () => {
+  const obsSubmissions = await loadObsAsJson();
+  const taskSubmissions = await loadTasksAsJson();
+  const submissions = Object.assign({}, obsSubmissions, taskSubmissions);
+  const list = document.getElementById("csv-list");
+  const elements = [];
+  taskSubmissions.forEach((row) => {
+    elements.push(row.Tasks);
+  });
+  const uniqueElements = [...new Set(elements)];
+  uniqueElements.forEach((value) => {
+    const li = document.createElement("li");
+    li.classList.add("interaction-element");
+    li.textContent = value;
+    list.appendChild(li);
+  });
+  document.querySelectorAll(".interaction-element").forEach((element, index) => {
+    element.addEventListener("click", () => {
+      const submission = submissions[index];
+      console.log(submission);
+      document.getElementById("modal-meta").innerHTML = `
+                        <p><strong>Name:</strong> ${submission.Tasks}</p>
+                        <p><strong>Description:</strong> ${submission.Description || "No description"}</p>
+                    
+                        `;
+      document.getElementById("gif-modal").classList.remove("hidden");
+    });
+    document.getElementById("close-modal").addEventListener("click", () => {
+      document.getElementById("gif-modal").classList.add("hidden");
+    });
+    document.getElementById("gif-modal").addEventListener("click", (e) => {
+      if (e.target.id === "gif-modal") {
+        document.getElementById("gif-modal").classList.add("hidden");
+      }
+    });
+  });
+});
+//# sourceMappingURL=tasks-CzImV5uD.js.map
